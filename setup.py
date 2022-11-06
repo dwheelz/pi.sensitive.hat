18a29de98e245c72e4b33bf8747670d9f563ec16
Jumbo Bumbo <jamessomethingsomething@duck.com>
2022-11-06 23:44:52 +0000

    submodule RTIMULib (pip gets sad)

--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
 import os
+from pathlib import Path
 from setuptools import setup, find_packages
+import subprocess
+
+
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+# Config RTIMULib (its a right pain)
+def _setup_rtimulib():
+    rtimlib_cwd = Path("src", "RTIMULib", "Linux", "python").absolute()
+    subprocess.check_call(["python", "setup.py", "build"], cwd=rtimlib_cwd)
+    subprocess.check_call(["python", "setup.py", "install"], cwd=rtimlib_cwd)
+
+_setup_rtimulib()
 
 setup(
     name="pi.sensitive.hat",
