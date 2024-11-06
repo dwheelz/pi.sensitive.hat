9f882a6274f78416e2d04a397086fa7d14dc3ecf
anothersalad <48966874+another-salad@users.noreply.github.com>
2024-11-06 00:12:00 +0000

    tweaks and great lint stuff sure

--- src/sense_app/common/set_pixels.py
+++ src/sense_app/common/set_pixels.py
@@ -10,20 +10,27 @@ from sense_hat import SenseHat  # pylint: disable=import-error
 CLEAR = [[0, 0, 0] for _ in range(64)]
 
 
+# pylint: disable=too-few-public-methods
 class SetPattern:
+    """Base Pattern class"""
 
     def __init__(self, pattern: list) -> None:
+        """init"""
         self.pattern = pattern
         self.hat = SenseHat()
 
-    def clear(self):
+    def set(self) -> None:
+        "sets the pattern to the display"
+        self.hat.set_pixels(self.pattern)
+
+    def clear(self) -> None:
+        "clears the display"
         self.hat.set_pixels(CLEAR)
 
 
 class SetPatternFromJsonFile(SetPattern):
+    """Patterns from Json files"""
 
     def __init__(self, filepath: str) -> None:
+        """init"""
         super().__init__(loads(Path(filepath).read_text(encoding="utf-8")))
-
-    def set(self):
-        self.hat.set_pixels(self.pattern)
